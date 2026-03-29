1b5d7ee2c20c41724cce57a6122cff122423b4d3
AndresRios123 <andresc.rios@campusucc.edu.co>
2026-03-28 19:25:05 -0500

    feature/Agregada estructura Hero

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from "../components/layout/Navbar";
+import Hero from "../components/sections/Hero";
 
 export default function HomePage() {
   return (
     <main>
       <Navbar />
+      <Hero />
     </main>
   );
 }
---
+++ app/components/sections/Hero.tsx
@@ -0,0 +1,37 @@
+export default function Hero(){
+    return(
+        <section id="inicio">
+            <div>
+                <div>
+                    <p>Hola, mi nombre es</p>
+                    <h1>Andres Rios</h1>
+                    <h2>Ingeniero de Software</h2>
+                    <p>
+                        enfocado en desarrollar soluciones tecnológicas eficientes,
+                        escalables y bien diseñadas.
+                    </p>
+
+                    <div>
+                        <a href="/cv.pdf" download>
+                            Descargar CV
+                        </a>
+                    </div>
+
+                    <div>
+                        <a href="/">Github</a>
+                        <a href="/">LinkedIn</a>
+                        <a href="/">Instagram</a>
+                        <a href="/">Facebook</a>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <div>
+                            <span>Foto</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
